27e36ba4d54e8e89d951f86bef0cdc859d9e37b7
Zeikseiji <zeiksenpai6@gmail.com>
2025-05-16 11:02:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let userBox = document.querySelector('.lol .account-box');
+
+document.querySelector('#user-btn').onclick = () =>{
+    userBox.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.lol .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    userBox.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    userBox.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+
